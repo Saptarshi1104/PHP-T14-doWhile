6d738a36e5739f89123d749b753941e91c0cfa01
Saptarshi Mandal <80155876+Saptarshi1104@users.noreply.github.com>
2023-12-01 19:17:36 +0530

    Add files via upload

---
+++ doWhileLoop.php
@@ -0,0 +1,22 @@
+<?php
+echo "Welcome to do-While Loops <br>";
+
+// SYNTAX
+/*
+do{
+    some lines of code
+    .....
+} while(condition);
+*/
+
+$j = 89;
+do{
+    echo "$j <br>";
+} while($j<5);
+
+$i = 0;
+do{
+    echo "$i <br>";
+    $i++;
+} while($i<5);
+?>
